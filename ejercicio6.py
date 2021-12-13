a7065a05705b6e33a86abe2dc423edbe8b80d83b
k1llth3bug <ramonsanricio@hotmail.com>
2021-12-13 10:10:11 +0100

    Ejercicio 6 - Control de lista vacía

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,5 +1,7 @@
 lista_numeros = []
 while (num := int(input("Dime un número: "))) >=0:
-    if num>=0:
-        lista_numeros.append(num)
-print(f"El valor medio de los números es: {sum(lista_numeros)/len(lista_numeros)}")
+    lista_numeros.append(num)
+if len(lista_numeros) == 0:
+    print("No ha introducido nungún número no negativo")
+else:
+    print(f"El valor medio de los números es: {sum(lista_numeros)/len(lista_numeros)}")
