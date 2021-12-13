2f82ed140c7f1c8ae5e28d85a945251fa33e8b00
k1llth3bug <ramonsanricio@hotmail.com>
2021-12-13 11:18:53 +0100

    Ejercicio 6 - Corregida errata

--- sge_t05p01_e06_SanchezRamon.py
+++ sge_t05p01_e06_SanchezRamon.py
@@ -2,6 +2,6 @@ lista_numeros = []
 while (num := int(input("Dime un número: "))) >=0:
     lista_numeros.append(num)
 if len(lista_numeros) == 0:
-    print("No ha introducido nungún número no negativo")
+    print("No ha introducido ningún número no negativo")
 else:
     print(f"El valor medio de los números es: {sum(lista_numeros)/len(lista_numeros)}")
