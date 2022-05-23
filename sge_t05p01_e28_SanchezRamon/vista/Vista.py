226176cdf4640966f1e8bc1a8612246367339a66
k1llth3bug <ramonsanricio@hotmail.com>
2022-05-23 11:18:58 +0200

    Mejorado mostrado de opciones

--- sge_t05p01_e28_SanchezRamon/vista/Vista.py
+++ sge_t05p01_e28_SanchezRamon/vista/Vista.py
@@ -36,12 +36,12 @@ class Vista:
 
     def mostrar_opciones_admin(self):
         opcion = -1
+        lista_opciones = ["1. Ver listado completo de socios.","2. Insertar un nuevo socio.",
+        "3. Añadir a un socio su familia.","4. Ver listado completo de los próximos eventos.",
+        "5. Buscar eventos por fecha y ver la información.","6. Insertar un nuevo evento.",
+        "7. Ver el control de cuotas-socios anual.","8. Actualizar control de cuotas-socios.",
+        "9. Realizar el pago de una cuota por DNI del socio.", "0. Salir"]
         while opcion != 0:
-            lista_opciones = ["1. Ver listado completo de socios.","2. Insertar un nuevo socio.",
-            "3. Añadir a un socio su familia.","4. Ver listado completo de los próximos eventos.",
-            "5. Buscar eventos por fecha y ver la información.","6. Insertar un nuevo evento.",
-            "7. Ver el control de cuotas-socios anual.","8. Actualizar control de cuotas-socios.",
-            "9. Realizar el pago de una cuota por DNI del socio.", "0. Salir"]
             print("\n".join(lista_opciones))
             print("Dime una opción: ")
             opcion = self.pedir_entero(0, len(lista_opciones)-1)
@@ -49,11 +49,11 @@ class Vista:
 
     def mostrar_opciones_socio(self):
         opcion = -1
+        lista_opciones = ["1. Ver mis próximos eventos y lista de inscritos.","2. Inscribirme a evento.",
+        "3. Ver mis bicicletas .","4. Ver mis reparaciones.", "5. Añadir nueva bicicleta.",
+        "6. Añadir reparación a bicicleta.", "7. Ver mi familia.", "8. Ver mi histórico.",
+        "0. Salir"]
         while opcion != 0:
-            lista_opciones = ["1. Ver mis próximos eventos y lista de inscritos.","2. Inscribirme a evento.",
-            "3. Ver mis bicicletas .","4. Ver mis reparaciones.", "5. Añadir nueva bicicleta.",
-            "6. Añadir reparación a bicicleta.", "7. Ver mi familia.", "8. Ver mi histórico.",
-            "0. Salir"]
             print("\n".join(lista_opciones))
             print("Dime una opción: ")
             opcion = self.pedir_entero(0, len(lista_opciones)-1)
