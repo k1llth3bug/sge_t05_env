37ccdd34299b4dabbdd1e2487ed73ece05fc8d57
k1llth3bug <ramonsanricio@hotmail.com>
2022-06-13 01:07:41 +0200

    Formateado Autobus.py

--- sge_t05p01_e28_SanchezRamon/modelo/Autobus.py
+++ sge_t05p01_e28_SanchezRamon/modelo/Autobus.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 class Autobus:
-    def __init__(self, fecha_tope: date,carrera: str, min_plazas: int, max_plazas: int, nombre: str) -> None:
+    def __init__(self, fecha_tope: date, carrera: str, min_plazas: int, max_plazas: int, nombre: str) -> None:
         self.__fecha_tope, self.__carrera, self.__min_plazas, self.__max_plazas, self.__nombre = fecha_tope, carrera, min_plazas, max_plazas, nombre
 
     def __repr__(self) -> str:
